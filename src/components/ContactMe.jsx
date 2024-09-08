85f87eaef9e55ed4a756f8a9d9987cd0943c9d24
JuanJo5433 <juanjosegiraldo1001@gmail.com>
2024-09-08 13:29:56 -0500

    Fix: Update more text button in experience

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,60 +1,126 @@
-import React from "react";
+import React, { useRef } from "react";
 import Title from "./Title";
 import "./scss/ContactMe.scss";
+import emailjs from "@emailjs/browser";
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+
 
 const ContactMe = () => {
+    const serviceID = "default_service";
+    const templateID = "template_20so0xm";
+    const publicKeyID = "moJbnxicpB91S_Poz";
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(serviceID, templateID, form.current, {
+                publicKey: publicKeyID,
+            })
+            .then(
+                () => {
+                    console.log("SUCCESS!");
+                    Swal.fire({
+                        // position: "top-end",
+                        icon: "success",
+                        title: "El correo fue enviado correctamente",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                      form.current.reset();
+                },
+                (error) => {
+                    console.log("FAILED...", error.text);
+                    Swal.fire({
+                        // position: "top-end",
+                        icon: "error",
+                        title: "El correo no fue enviado, Intenta nuevamente",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
+            );
+    };
+
     return (
         <div className="contactMeContainer" data-aos="flip-up">
             <Title title={"Contacto"} />
             <div className="contactMeContent">
                 <div className="form-card1">
                     <div className="form-card2">
-                        <form className="form">
-
+                        <form
+                            className="form"
+                            id="form"
+                            onSubmit={sendEmail}
+                            ref={form}
+                        >
                             <div className="form-field">
                                 <input
-                                    required=""
-                                    placeholder="Name"
+                                    required
+                                    placeholder="Nombre"
                                     className="input-field"
                                     type="text"
+                                    name="name"
+                                    id="name"
                                 />
                             </div>
 
                             <div className="form-field">
                                 <input
-                                    required=""
-                                    placeholder="Email"
+                                    required
+                                    placeholder="Correo Electronico"
                                     className="input-field"
                                     type="email"
+                                    name="email"
+                                    id="email"
                                 />
                             </div>
 
                             <div className="form-field">
                                 <input
-                                    required=""
-                                    placeholder="Subject"
+                                    required
+                                    placeholder="Asunto"
                                     className="input-field"
                                     type="text"
+                                    name="subject"
+                                    id="subject"
                                 />
                             </div>
 
                             <div className="form-field">
                                 <textarea
-                                    required=""
-                                    placeholder="Message"
+                                    required
+                                    placeholder="Mensaje"
                                     cols="30"
                                     rows="10"
                                     className="input-field"
+                                    name="message"
+                                    id="message"
                                 ></textarea>
                             </div>
 
-                            <button className="sendMessage-btn">
+                            <button
+                                className="sendMessage-btn"
+                                type="submit"
+                                id="button"
+                                value="Send Email"
+                            >
                                 Send Message
                             </button>
                         </form>
                     </div>
                 </div>
             </div>
+
+            <script
+                type="text/javascript"
+                src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"
+            ></script>
+
+            <script type="text/javascript">
+                emailjs.init('moJbnxicpB91S_Poz')
+            </script>
         </div>
     );
 };
